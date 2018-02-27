ff618c1aab6424432b27eb092787c131b17bf4ce
z0glen <zgbauer@gmail.com>
2018-02-27 13:16:21 -0500

    uploading resources

---
+++ run.py
@@ -0,0 +1,13 @@
+#!flask/bin/python
+from app import app
+app.run(debug=True)
+
+"""
+TODO list:
+- add blocking users
+- add filter posts displayed by custom groups
+- better full text search engine? (e.g. haystack for django)
+- full text doesn't work in python 3
+- cosmetic changes (bootstrap)
+- translations (?)
+"""
