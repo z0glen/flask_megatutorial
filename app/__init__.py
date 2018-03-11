0c9778c1ce63f96b6019b23c75a8d7a5e814ca55
z0glen <zgbauer@gmail.com>
2018-03-10 19:42:00 -0500

    first four chapters of revised content

--- app/__init__.py
+++ app/__init__.py
@@ -1,44 +1,11 @@
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
-import os
-from flask_login import LoginManager
-from flask_openid import OpenID
-from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
-from flask_mail import Mail
-from .momentjs import momentjs
-from flask_babel import Babel
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(Config)
 db = SQLAlchemy(app)
-mail = Mail(app)
-babel = Babel(app)
+migrate = Migrate(app, db)
 
-lm = LoginManager()
-lm.init_app(app)
-lm.login_view = 'login'
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
-
-app.jinja_env.globals['momentjs'] = momentjs
-
-if not app.debug:
-	import logging
-	from logging.handlers import SMTPHandler
-	credentials = None
-	if MAIL_USERNAME or MAIL_PASSWORD:
-		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-	mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
-	mail_handler.setLevel(logging.ERROR)
-	app.logger.addHandler(mail_handler)
-	
-if not app.debug:
-	import logging
-	from logging.handlers import RotatingFileHandler
-	file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
-	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-	app.logger.setLevel(logging.INFO)
-	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_handler)
-	app.logger.info('microblog startup')
-
-from app import views, models
+from app import routes, models
--- config.py
+++ config.py
@@ -1,40 +1,8 @@
-# -*- coding: utf-8 -*-
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess-dis-password'
-
-OPENID_PROVIDERS = [
-	{'name': 'Google', 'url': 'https://www.google.com/accounts/08/id'},
-	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-	{'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-	{'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-	{'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
-]
-
-# email server
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
-MAIL_USERNAME = '<email>'
-MAIL_PASSWORD = '<password>'
-
-# administrator list
-ADMINS = ['<email>']
-
-# pagination
-POSTS_PER_PAGE = 3
-
-WHOOSH_BASE = os.path.join(basedir, 'search.db')
-MAX_SEARCH_RESULTS = 50
-
-#available languages
-LANGUAGES = {
-	'en': 'English',
-	'es': 'Español'
-}
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
